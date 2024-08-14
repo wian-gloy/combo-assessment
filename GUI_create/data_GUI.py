d74aa1cccae9ae53afa5cac5af58ff00a1ec22d3
wian-gloy <wian.gloy@npbhs.school.nz>
2024-08-15 11:56:07 +1200

    home page UI

--- GUI_create/data_GUI.py
+++ GUI_create/data_GUI.py
@@ -7,4 +7,9 @@ buttons=["View Database","Add Item","Remove Item","Edit Items","Close"]
 
 rating=["G",'PG','M','R13','R16','R18','RP13','RP16','R']
 
+eg.msgbox("Welcome to the movie the database.",title="Welcome")
 
+action=eg.buttonbox("home page",choices=buttons)
+
+if action== buttons[0]:
+    print('asd')
