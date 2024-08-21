981a85ba14e9b7cbda5051f8df783ee092986a72
wian-gloy <wian.gloy@npbhs.school.nz>
2024-08-22 10:35:39 +1200

    tkinter view database with pandas

--- trialing/view.py
+++ trialing/view.py
@@ -1,6 +1,23 @@
 import sqlite3
 import pandas as pd
 import easygui as eg
+import tkinter as tk 
+from tkinter.scrolledtext import ScrolledText
+
+
+def show_msg(title,message):
+    root = tk.Tk()
+    root.title(title)
+
+    text_widget = ScrolledText(root, wrap=tk.WORD, width=100, height=30)
+    text_widget.pack(fill=tk.BOTH, expand=True)
+    
+    text_widget.insert(tk.END, message)
+    text_widget.config(state=tk.DISABLED)
+    
+
+    root.mainloop()
+
 
 conn=sqlite3.connect("Films.db")
 cursor=conn.cursor()
@@ -12,5 +29,11 @@ rating=["G",'PG', 'M','R13','R16','R18','RP13','RP16','R']
 eg.msgbox("Welcome to the movie the database.",title="Welcome")
 
 action=eg.buttonbox("home page",choices=buttons)
- 
-df=pd.read
+
+if action==buttons[0]:
+    data = pd.read_sql_query('Select * from FILMS ORDER BY Film_ID ASC;', conn)
+    
+    data_str = data.to_string(index=False)
+
+    show_msg("Show Database",data_str)
+
